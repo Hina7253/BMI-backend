0401e42f283a99f768543a851aabf5fa375f7d35
Hina <hinakumari.725394@gmail.com>
2026-02-20 22:18:31 +0530

    Update BmiController.java

--- bmi-backend-only/src/main/java/com/bmi/controller/BmiController.java
+++ bmi-backend-only/src/main/java/com/bmi/controller/BmiController.java
@@ -10,7 +10,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
