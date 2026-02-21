5c61ac171ec52b74f77637a4ea71f78821cdf306
Hina <hinakumari.725394@gmail.com>
2026-02-21 22:21:02 +0530

    Update BmiRequest.java
    Removed weight field from BmiRequest class.

--- bmi-backend-only/src/main/java/com/bmi/model/BmiRequest.java
+++ bmi-backend-only/src/main/java/com/bmi/model/BmiRequest.java
@@ -7,7 +7,6 @@ public class BmiRequest {
     
     @NotNull(message = "Weight is required")
     @Positive(message = "Weight must be positive")
-    private Double weight;
     
     @NotNull(message = "Height is required")
     @Positive(message = "Height must be positive")
